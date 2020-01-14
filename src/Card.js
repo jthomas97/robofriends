58a59b992a73f230d949d1a4cc6cf5f3e90df8c2
Jude <judethomas97@yahoo.com>
2020-01-14 15:16:42 -0800

    completed Build react app 2

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-const Card = (props) => {
+// index.js passes robots array aka 'props' to CardList.js
+// CardList.js passes individual robots to Card
+// Card function accepts them as paramaters and destructures them
+// since they are passed as a object
+// So you dont have to do props.name props.email etc...
+const Card = ({name, email, id}) => {
   return (
     <div className=' tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src={`https://robohash.org/${props.id}?200x200`} alt='robot' / >
+      <img src={`https://robohash.org/${id}?200x200`} alt='robot' / >
       <div>
-        <h2>{props.name}</h2>
-        <p>{props.email}</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
