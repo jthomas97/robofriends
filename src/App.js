f385d1039f905dcdd374baaf16b985a46a22e87c
Jude <judethomas97@yahoo.com>
2020-01-14 20:22:55 -0800

    basic version of robofriends  completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import {robots} from './robots'
 
-const App = ({robots}) => {
-  return (
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots}/>
-    </div>
-  );
+import './App.css';
+
+
+
+// Each state component must have a render function that returns something
+
+// To add state, must have a constructor
+
+// To use the keyword 'this' you must call super.
+// Which basically calls the constructor of the Component obj
+class App extends React.Component {
+  constructor(){
+    super()
+  // Set initail state robots as the imported robots array,
+  // Set initial state for searchfield as empty str
+    this.state = {
+      robots: robots,
+      searchfield: ""
+    }
+  }
+
+//When input from SearchBox component is changed,
+// Call this function.
+// Which updates state field as the target.value from event
+
+//onSearchChange needs to be arrow function, since this.state refers to this object,
+//Without arrow function, this.state would be trying to get it from input
+
+// Rule of thumb: when you make your own methods on components, make them arrow funcs
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value})
+  }
+
+// We are able to update the robots since when state changes render function is re-ran
+  render(){
+  // Take this.state.robots array and filter, only the names in the searchbox
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    return (
+      <div className='tc'>
+        <h1 className='f2'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+    {/* Initially filteredRobots is an array of all the robots since
+    it initally compare if each robot.name includes a string. Which is obv true */}
+        <CardList robots={filteredRobots}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const SearchBox = () => {
+
+// searchChange is a function that is passed to component SearchBox
+const SearchBox = ({searchChange}) => {
   return (
     <div className='pa2'>
       <input 
         type='search' 
         placeholder='search robots' 
         className='pa3 ba b--green bg-lightest-blue'
+      // Basic HTML, when input field is changed, call function searchChange
+        onChange={searchChange}
       />
     </div>
   );
